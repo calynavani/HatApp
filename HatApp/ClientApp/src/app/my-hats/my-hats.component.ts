9277d3f51beaf3e1ebd65c2e7462e71200541ee3
James Mitchell <jfalmogusa@gmail.com>
2022-02-04 17:22:34 -0500

    Added additional looper functionality

--- HatApp/ClientApp/src/app/my-hats/my-hats.component.ts
+++ HatApp/ClientApp/src/app/my-hats/my-hats.component.ts
@@ -2,7 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {UserObject, UOConvert} from '../UserObject';
 import { UserService } from '../user.service';
-import { Hat } from '../Hat';
+import { Hat, HatConvert } from '../Hat';
+import { HatService } from '../hat.service';
 
 
 @Component({
@@ -13,22 +14,34 @@ import { Hat } from '../Hat';
 })
 export class MyHatsComponent implements OnInit {
 currentUser:UserObject;
+randomHat: Hat;
+allHats: Hat[]=[];
+
 id = Number(this.route.snapshot.paramMap.get('id'));
-  constructor(private userService: UserService, private route: ActivatedRoute) {
+  constructor(private userService: UserService, private route: ActivatedRoute, private hatService: HatService) {
   this.userService.GetUserObject(this.id).subscribe(
     (response: any)=>{
       let json = UOConvert.userObjectToJson(response);
       this.currentUser = UOConvert.toUserObject(json);      
     }
-  )
-
+  );
+  this.hatService.GetAllHats().subscribe(
+    (response: any)=>{
+      let json = HatConvert.hatToJson(response);
+      this.allHats = HatConvert.toHat(json);
+    }
+  );
 }
 
 ngOnInit() {
 }
 randomHatPicker(){
-  let target=document.getElementById("randomDiv");
-  for(let i: number=0;i<=this.currentUser.myCollection.length+1;i++){}
+  let target=(<HTMLImageElement>document.getElementById("randomDiv"));
+  let i = Math.floor(Math.random())
+  for(let i: number=0;i<=this.allHats.length;i++){    
+    target.innerHTML='<img id="imgtarget" src="'+this.currentUser.myCollection[i].images+'"/>';
+    console.log(this.currentUser.myCollection[i].images);
+  }
 }
 
 }
